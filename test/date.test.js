5fbc4868399a5a904397b8a28d2d3e4ade1566e6
LostStars <453839615@qq.com>
2018-04-27 17:25:41 +0800

    mocha

--- test/date.test.js
+++ test/date.test.js
@@ -2,12 +2,6 @@ var today = require('../date.js').today;
 var expect = require('chai').expect;
 
 describe('测试日期函数today', function(){
-    before(function(){
-        console.log('before');
-    });
-    after(function(){
-        console.log('after');
-    });
     it('返回值应当是当前年份', function(){
         let d = new Date();
         let date = d.getFullYear();
---
+++ test/timeout.test.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+
+describe('异步测试', function(){
+    it('测试应该在4000ms后结束', function(done){
+        var b = true;
+        function fn(){
+            b = false;
+            expect(b).to.be.not.ok;
+            done();
+        }
+        setTimeout(fn, 4000);
+    })
+})
