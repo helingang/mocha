c2b4ed5b7ca8b9d67c99539903dcc896b5ba1ab0
LostStars <453839615@qq.com>
2018-04-27 17:18:03 +0800

    mocha

--- test/date.test.js
+++ test/date.test.js
@@ -2,14 +2,20 @@ var today = require('../date.js').today;
 var expect = require('chai').expect;
 
 describe('测试日期函数today', function(){
+    before(function(){
+        console.log('before');
+    });
+    after(function(){
+        console.log('after');
+    });
     it('返回值应当是当前年份', function(){
         let d = new Date();
         let date = d.getFullYear();
         expect(today()).to.be.equal(date);
     });
-    it('是否返回2015', function(){
+    it('年份是否返回2015', function(){
         let d = new Date();
         let date = d.getFullYear();
         expect(today()).to.be.equal(2015);
-    })
+    });
 })
