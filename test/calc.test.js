919c15ec27177e37126791d24513168a79be6a88
LostStars <453839615@qq.com>
2018-04-27 15:39:43 +0800

    mocha

--- test/calc.test.js
+++ test/calc.test.js
@@ -5,11 +5,7 @@ describe('加法函数的测试', function(){
     it('测试1+2=3', function(){
         expect(add(1, 2)).to.be.equal(3);
     });
-    it('测试3+4=7', function(){
-        expect(add(3, 4)).to.be.equal(7);
+    it('测试返回的结果的类型是不是number', function(){
+        expect(add(3, 4)).to.match(/\d/);
     });
-    it('测试2+2=4', function(){
-        expect(add(2, 2)).to.be.equal(4);
-    });
-
 })
